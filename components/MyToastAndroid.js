54fbcf78ecf8d369e39115250e7ccdf0bade4d95
DarkermanDiaz <67664223+DarkermanDiaz@users.noreply.github.com>
2024-08-19 17:37:46 -0600

    toast

--- components/MyToastAndroid.js
+++ components/MyToastAndroid.js
@@ -3,13 +3,3 @@ import { StyleSheet, ToastAndroid, StatusBar } from 'react-native';
 export default function MyToastAndroid(msg) {
     ToastAndroid.show(msg, ToastAndroid.SHORT);
   }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingTop: StatusBar.currentHeight,
-      backgroundColor: '#6638f0',
-      padding: 8,
-    },
-  });
